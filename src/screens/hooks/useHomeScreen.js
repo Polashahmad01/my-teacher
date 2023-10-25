2fa4a144072366d5df0b884556ddc613525a1963
Polashahmad01 <dev.polashahmad@gmail.com>
2023-10-26 03:44:31 +0600

    add loader

--- src/screens/hooks/useHomeScreen.js
+++ src/screens/hooks/useHomeScreen.js
@@ -6,19 +6,20 @@ export const useHomeScreen = () => {
   const [teachersList, setTeachersList] = useState([])
   const [studentData, setStudentData] = useState({})
   const [recentTeacherData, setRecentTeacherData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isTeacherListLoading, setIsTeacherListLoading] = useState(false)
+  const [isStudentDataLoading, setIsStudentDataLoading] = useState(false)
+  const [isRecentTeacherDataLoading, setIsRecentTeacherDataLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState(null)
   const [showFullRecentTeacherData, setShowFullRecentTeacherData] = useState(false)
   const recentTeacherDataToShow = showFullRecentTeacherData ? recentTeacherData : recentTeacherData.slice(0, 5)
 
   const fetchOrderData = async () => {
     try {
-      setIsLoading(true)
+      setIsTeacherListLoading(true)
       const response = await getOrdersData()
       setTeachersList(response.content)
-      setIsLoading(false)
+      setIsTeacherListLoading(false)
     } catch (error) {
-      setIsLoading(false)
       console.error("Error: ", error)
       setErrorMessage(error.code)
     }
@@ -26,34 +27,34 @@ export const useHomeScreen = () => {
 
     const fetchCurrentUser = async () => {
     try {
-      setIsLoading(true)
+      setIsStudentDataLoading(true)
       const response = await getCurrentUser()
       setStudentData(response)
-      setIsLoading(false)
+      setIsStudentDataLoading(false)
     } catch(error) {
       console.error("Error: ", error)
       setErrorMessage(error.code)
     }
   }
   
-  const showLoadingUIForTableOfTeacher = isLoading
-  const showErrorUIForTableOfTeacher = !isLoading && teachersList.length === 0 && errorMessage !== null
-  const showUIForTableOfTeacher = !isLoading && teachersList.length > 0 && errorMessage === null
+  const showLoadingUIForTableOfTeacher = isTeacherListLoading
+  const showErrorUIForTableOfTeacher = !isTeacherListLoading && teachersList.length === 0 && errorMessage !== null
+  const showUIForTableOfTeacher = !isTeacherListLoading && teachersList.length > 0 && errorMessage === null
 
-  const showLoadingUIForStudentDetails = isLoading
-  const showErrorUIForStudentDetails = !isLoading && errorMessage !== null
-  const showUIForStudentDetails = !isLoading && Object.keys(studentData).length > 0 && errorMessage === null
+  const showLoadingUIForStudentDetails = isStudentDataLoading
+  const showErrorUIForStudentDetails = !isStudentDataLoading && errorMessage !== null
+  const showUIForStudentDetails = !isStudentDataLoading && Object.keys(studentData).length > 0 && errorMessage === null
 
-  const showLoadingUIForRecentTeacher = isLoading
-  const showErrorUIForRecentTeacher = !isLoading && recentTeacherData.length === 0 && errorMessage !== null
-  const showUIForRecentTeacher = !isLoading && recentTeacherData.length > 0 && errorMessage === null
+  const showLoadingUIForRecentTeacher = isRecentTeacherDataLoading
+  const showErrorUIForRecentTeacher = !isRecentTeacherDataLoading && recentTeacherData.length === 0 && errorMessage !== null
+  const showUIForRecentTeacher = !isRecentTeacherDataLoading && recentTeacherData.length > 0 && errorMessage === null
 
   const fetchTeacherData = async () => {
     try {
-      setIsLoading(true)
+      setIsRecentTeacherDataLoading(true)
       const response = await getTeachersData()
       setRecentTeacherData(response)
-      setIsLoading(false)
+      setIsRecentTeacherDataLoading(false)
     } catch(error) {
       console.error("Error: ", error)
       setErrorMessage(error.code)
